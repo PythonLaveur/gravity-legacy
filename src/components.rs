9027fcc1c781ae0da13ae90ce323c44379aa5af8
Adriao <mickael.adriao@telecom-paris.fr>
2022-06-26 22:53:14 +0200

    Importation of LDtk and beginning of mouvements

--- src/components.rs
+++ src/components.rs
@@ -1,4 +1,4 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, math::vec2};
 use bevy_ecs_ldtk::prelude::*;
 
 use std::collections::HashSet;
@@ -18,8 +18,8 @@ pub struct ColliderBundle {
 impl From<EntityInstance> for ColliderBundle {
     fn from(entity_instance: EntityInstance) -> ColliderBundle {
         let rotation_constraints = RotationConstraints::lock();
-
-        match entity_instance.identifier.as_ref() {
+        println!("Goes into from entity_instance of : {:?}", entity_instance.identifier);
+        match entity_instance.identifier.as_str() {
             "Player" => ColliderBundle {
                 collider: CollisionShape::Cuboid {
                     half_extends: Vec3::new(6., 14., 0.),
@@ -34,7 +34,7 @@ impl From<EntityInstance> for ColliderBundle {
                     half_extends: Vec3::new(5., 5., 0.),
                     border_radius: None,
                 },
-                rigid_body: RigidBody::KinematicVelocityBased,
+                rigid_body: RigidBody::Dynamic,
                 rotation_constraints,
                 ..Default::default()
             },
@@ -59,9 +59,10 @@ impl From<EntityInstance> for ColliderBundle {
 
 impl From<IntGridCell> for ColliderBundle {
     fn from(int_grid_cell: IntGridCell) -> ColliderBundle {
+        println!("Setup grid");
         let rotation_constraints = RotationConstraints::lock();
 
-        if int_grid_cell.value == 2 {
+        if int_grid_cell.value == 2 ||int_grid_cell.value == 1 {
             ColliderBundle {
                 collider: CollisionShape::Cuboid {
                     half_extends: Vec3::new(8., 8., 0.),
@@ -79,18 +80,32 @@ impl From<IntGridCell> for ColliderBundle {
 
 // endregion: --- common structs
 
-#[derive(Copy, Clone, Eq, PartialEq, Debug, Default, Component)]
-pub struct Player;
+#[derive(Copy, Clone, PartialEq, Debug, Component)]
+pub struct Player{
+    pub previous_input : Vec2
+}
+impl Default for Player {
+    fn default() -> Self {
+       Player {
+        previous_input : Vec2::new(0., 0.)
+       }
+    }
+}
 
 #[derive(Clone, Default, Bundle, LdtkEntity)]
 pub struct PlayerBundle {
+    #[sprite_bundle("player.png")]
     #[bundle]
     pub sprite_bundle: SpriteBundle,
+    #[from_entity_instance]
     #[bundle]
     pub collider_bundle: ColliderBundle,
     pub player: Player,
+    #[worldly]
+    pub worldly: Worldly,
     pub ground_detection: GroundDetection,
-
+    
+    
     // The whole EntityInstance can be stored directly as an EntityInstance component
     #[from_entity_instance]
     entity_instance: EntityInstance,
@@ -102,30 +117,6 @@ pub struct Wall;
 #[derive(Clone, Debug, Default, Bundle, LdtkIntCell)]
 pub struct WallBundle {
     wall: Wall,
-}
-
-#[derive(Copy, Clone, Eq, PartialEq, Debug, Default, Component)]
-pub struct Floor;
-
-#[derive(Clone, Debug, Default, Bundle, LdtkIntCell)]
-pub struct FloorBundle {
-    floor: Floor,
-}
-
-#[derive(Clone, Default, Bundle, LdtkEntity)]
-pub struct PotBundle {
-    #[bundle]
-    pub sprite_sheet_bundle: SpriteBundle,
-    #[bundle]
-    pub collider_bundle: ColliderBundle,
-}
-
-#[derive(Clone, Default, Bundle, LdtkEntity)]
-pub struct KeyBundle {
-    #[bundle]
-    pub sprite_sheet_bundle: SpriteBundle,
-    #[bundle]
-    pub collider_bundle: ColliderBundle,
 }
 
 #[derive(Clone, Default, Component)]
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
-// This example shows off a more in-depth implementation of a game with `bevy_ecs_ldtk`.
-// Please run with `--release`.
-
 use bevy::prelude::*;
 use bevy_ecs_ldtk::prelude::*;
 
+//use components::PlayerBundle;
 use heron::prelude::*;
+use systems::print_player_position;
+//use systems::{process_my_entities};
 
 mod components;
 mod systems;
@@ -13,16 +13,22 @@ mod systems;
 
 const MAP_LDTK: &str = "test.ldtk";
 
+const TILE_SIZE: f32 = 16.;
+
 // endregion: --- Assets constants
 
 fn main() {
     App::new()
+        .insert_resource(WindowDescriptor {
+            title: "RustyGame!".to_string(),
+            width: 500.0,
+            height: 300.0,
+            ..Default::default()
+        })
         .add_plugins(DefaultPlugins)
         .add_plugin(LdtkPlugin)
         .add_plugin(PhysicsPlugin::default())
-        //Gravity parameter
-        .insert_resource(Gravity::from(Vec3::new(0.0, -2000., 0.0)))
-        //Select the lvl to compute
+        .insert_resource(Gravity::from(Vec3::new(0.0, -10., 0.0)))
         .insert_resource(LevelSelection::Index(0))
         .insert_resource(LdtkSettings {
             level_spawn_behavior: LevelSpawnBehavior::UseWorldTranslation {
@@ -32,13 +38,15 @@ fn main() {
             ..Default::default()
         })
         .add_startup_system(systems::setup)
+        //.add_startup_system(process_my_entities)
+        .add_system(systems::spawn_wall_collision)
+        .add_system(print_player_position)
+        
         // Map the components to match project structs
         // Tiles
         .register_ldtk_int_cell::<components::WallBundle>(1)
-        .register_ldtk_int_cell::<components::FloorBundle>(2)
+        .register_ldtk_int_cell::<components::WallBundle>(2)
         //Entities
         .register_ldtk_entity::<components::PlayerBundle>("Player")
-        .register_ldtk_entity::<components::PotBundle>("Pot")
-        .register_ldtk_entity::<components::KeyBundle>("Key")
         .run();
 }
