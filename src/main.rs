65b10284244a26ea6d98f24fa2f20f1422b7b91f
Xia Jiaqi <xiajiaqi2001@gmail.com>
2022-06-27 19:33:39 +0200

    menu

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,40 @@
+#![allow(clippy::redundant_field_names)]
+#![allow(clippy::too_many_arguments)]
 use bevy::prelude::*;
 use bevy_ecs_ldtk::prelude::*;
-
+use std::env;
 use bevy_kira_audio::AudioPlugin;
 //use components::PlayerBundle;
 use heron::prelude::*;
-use systems::{input_player_movement, world_rotation_system, background_audio};
+use systems::*;
+use ascii::AsciiPlugin;
+use fadeout::FadeoutPlugin;
 //use systems::{process_my_entities};
-
+use start_menu::MainMenuPlugin;
 mod components;
 mod systems;
+mod start_menu;
+mod ascii;
+mod fadeout;
 
 // region:    --- Assets constants
-
+pub const TILE_SIZE: f32 = 0.1;
 const MAP_LDTK: &str = "test.ldtk";
+pub const CLEAR: Color = Color::rgb(0.1, 0.1, 0.1);
 
 //const TILE_SIZE: f32 = 16.;
-
+#[derive(Debug, Clone, PartialEq, Eq, Hash, Copy)]
+pub enum GameState {
+    StartMenu,
+    Overworld,
+    Combat,
+}
 // endregion: --- Assets constants
-
 fn main() {
+    env::set_var("RUST_BACKTRACE", "1");
     App::new()
+        .add_state(GameState ::StartMenu)
+        .insert_resource(ClearColor(CLEAR))
         .insert_resource(WindowDescriptor {
             title: "RustyGame!".to_string(),
             width: 500.0,
@@ -30,6 +45,9 @@ fn main() {
         .add_plugin(LdtkPlugin)
         .add_plugin(AudioPlugin)
         .add_plugin(PhysicsPlugin::default())
+        .add_plugin(FadeoutPlugin)
+        .add_plugin(AsciiPlugin)
+        .add_plugin(MainMenuPlugin)
         .insert_resource(Gravity::from(Vec3::new(0.0, -2000., 0.0)))
         .insert_resource(LevelSelection::Index(0))
         .insert_resource(LdtkSettings {
@@ -39,12 +57,14 @@ fn main() {
             set_clear_color: SetClearColor::FromLevelBackground,
             ..Default::default()
         })
-        .add_startup_system(systems::setup)
-        .add_startup_system(background_audio)
+        .add_system(systems::setup)
+        
+        .add_system_set(SystemSet::on_update(GameState::Overworld).with_system(world_rotation_system))
+        .add_system_set(SystemSet::on_update(GameState::Overworld).with_system(background_audio))
         //.add_startup_system(process_my_entities)
-        .add_system(world_rotation_system)
-        .add_system(systems::spawn_wall_collision)
-        .add_system(input_player_movement)
+        .add_plugin(MainMenuPlugin)
+        .add_system_set(SystemSet::on_update(GameState::Overworld).with_system(spawn_wall_collision))
+        .add_system_set(SystemSet::on_update(GameState::Overworld).with_system(input_player_movement))
         // Map the components to match project structs
         // Tiles
         .register_ldtk_int_cell::<components::WallBundle>(1)
@@ -53,3 +73,8 @@ fn main() {
         .register_ldtk_entity::<components::PlayerBundle>("Player")
         .run();
 }
+
+
+
+
+// system
